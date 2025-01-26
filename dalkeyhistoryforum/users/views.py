01de999d73d4a316f6ab6ef747aec6275b216afe
Amy Hastings <amy.hastings@gmail.com>
2025-01-26 15:12:58 +0000

    Added user registration and authentication

---
+++ dalkeyhistoryforum/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'The account {username} was created successfully, now you can login.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm() 
+    return render(request, 'users/register.html', {'form': form})
+
