4504b3f68812383318ed291d80e5bd77bea6b944
Amy Hastings <amy.hastings@gmail.com>
2025-02-13 17:30:24 +0000

    Changed Allowed Hosts

--- dalkeyhistoryforum/dalkeyhistoryforum/settings.py
+++ dalkeyhistoryforum/dalkeyhistoryforum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+l1png0gqdwbcf!6rgk(fe1n4rfyw$*ur784_copp$1sm47j+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
 
 # Application definition
